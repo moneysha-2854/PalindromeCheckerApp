e099e204345a78e3305aafcda4405e1db3f9187c
moneysha-2854 <mk2854@srmist.edu.in>
2026-02-25 14:26:13 +0530

    [MANISHA] Added UC2-HardCodePalindrome

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,5 +1,21 @@
 public class PalindromeCheckerApp {
     public static void main(String[] args){
-        System.out.ptintln("Welcome to Palindrome Checker App Management System");
+        System.out.println("Welcome to Palindrome Checker App Management System");
+        // Hardcoded string
+        String word = "madam";
+
+        // Reverse the string
+        String reversed = "";
+
+        for (int i = word.length() - 1; i >= 0; i--) {
+            reversed = reversed + word.charAt(i);
+        }
+
+        // Check if palindrome
+        if (word.equals(reversed)) {
+            System.out.println(word + " is a Palindrome.");
+        } else {
+            System.out.println(word + " is NOT a Palindrome.");
+        }
     }
 }
